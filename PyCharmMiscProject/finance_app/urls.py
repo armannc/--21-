f67843368670143f627e778ef1ea9b97c9f7e842
armannc <akmurat.arman2007@gmail.com>
2025-12-09 23:32:33 +0500

    Delete PyCharmMiscProject directory

--- PyCharmMiscProject/finance_app/urls.py
+++
@@ -1,8 +0,0 @@
-# finance_app/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(('finance_app.expenses.urls', 'expenses'), namespace='expenses')),
-]
